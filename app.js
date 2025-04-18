1b379b4e467bb65aba96ed33fda859dd826e9c03
Nick Dimizas <nikkosdim@gmail.com>
2025-04-18 16:39:18 +0300

    feat: renaming

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const requestLogger = require('./middlewares/request.logger');
-const errorHandler = require('./middlewares/error.handler');
-const registerRoutes = require('./routes/auth.routes');
+const helmet = require('helmet');
+const cors = require('cors');
+const requestLogger = require('../middlewares/request.logger');
+const errorHandler = require('../middlewares/error.handler');
+const authRoutes = require('../routes/auth.routes');
 
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
 
 
 app.use(requestLogger);
@@ -16,7 +20,7 @@ app.use(requestLogger);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/', authRoutes);
+app.use('/api/auth', authRoutes);
 
 app.use(errorHandler);
 
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const STATUS = require('../enums/status-codes');
 const User = require('../models/user.model');
 const { validateRegisterData } = require('../validators/auth.validator');
 const { checkUserExists } = require('../services/auth.service');
@@ -14,7 +15,7 @@ const registerUser = async (req, res, next) => {
     const newUser = new User({ username, email, password: hashedPassword });
 
     await newUser.save();
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(STATUS.CREATED).json({ message: 'User registered successfully' });
 
   } catch (err) {
     next(err);
